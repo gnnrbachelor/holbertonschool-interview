8569903d13cfc859ce992d73030e90033346a33a
gnnrbachelor <gnnrbachelor@gmail.com>
2022-01-06 17:47:48 +0000

    Add

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Minimum Operations """
+
+def minOperations(n):
+    """
+     min_opations
+    """
+    if n <= 1:
+        return 0
+    possible_op = 2
+    min_op = 0
+    quotient = n
+    while quotient > 1:
+        if (quotient % possible_op) == 0:
+            quotient = quotient // possible_op
+            min_op += possible_op
+        else:
+            possible_op += 1
+
+    return min_op
+
