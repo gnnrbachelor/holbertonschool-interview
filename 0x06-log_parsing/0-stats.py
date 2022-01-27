918377da6c10dc6adf89605432fd629e4e67a201
root <gnnrbachelor@gmail.com>
2022-01-27 18:31:29 +0000

    Factor out parse and print from main

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,35 +13,42 @@ stat = {
     "403": 0,
     "404": 0,
     "405": 0,
-    "500": 0
-}
-size = 0
+     "500": 0
+    }
 
-def printer():
+
+def printer(size):
     """Print function"""
     print("File size: {}".format(size))
     for status in sorted(stat.keys()):
         if stat[status]:
             print("{}: {}".format(status, stat[status]))
-    
 
-if __name__ == '__main__':
+
+def log_parse():
     """Progam to parse log"""
     count = 0
+    size = 0
+    for line in sys.stdin:
+        try:
+            info_list = line.split()
+            size += int(info_list[-1])
+            if info_list[-2] in stat:
+                stat[info_list[-2]] += 1
+        except:
+            pass
+        if count == 9:
+            printer(size)
+            count = -1
+        count += 1
+
+
+if __name__ == '__main__':
+    """Entry point"""
     try:
-        for line in sys.stdin:
-            try:
-                info_list = line.split()
-                size += int(info_list[-1])
-                if info_list[-2] in stat:
-                    stat[info_list[-2]] += 1
-            except:
-                pass
-            if count == 9:
-                printer()
-                count = -1
-            count += 1
+        log_parse()
     except KeyboardInterrupt:
-        printer()
+        log_parse()
         raise
-    printer()
+    log_parse()
+
