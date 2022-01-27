6225f245ac6f0c0c0020477e1709c22bdf8a1785
root <gnnrbachelor@gmail.com>
2022-01-27 17:03:13 +0000

    Add

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+
+stat = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+size = 0
+
+if __name__ == '__main__':
+    count = 0
+    for line in sys.stdin:
+        try:
+            info_list = line.split()
+            size += int(info_list[-1])
+            if info_list[-2] in stat:
+                stat[info_list[-2]] += 1
+        except:
+            pass
+        if count == 9:
+            print("File size: {}".format(size))
+            for status in sorted(stat.keys()):
+                if stat[status]:
+                    print("{}: {}".format(status, stat[status]))
+            count = -1
+        count += 1
