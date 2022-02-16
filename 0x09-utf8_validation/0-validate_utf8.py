192bc5343a169aad0cac34bd6fe5f1d99f2b89c5
root <gnnrbachelor@gmail.com>
2022-02-16 16:24:09 +0000

    Add first solution

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
-def validUTF8(data)
+def validUTF8(data):
+    """Checks for UTF-8 compliance"""
+    try:
+        # bytes.decode() default args included for clarity
+        bytes(data).decode(encoding='utf-8', errors='strict')
+    except (ValueError, UnicodeDecodeError):
+        return False
+    return True
 
 
