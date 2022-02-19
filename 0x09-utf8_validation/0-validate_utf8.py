a557d708512d13053a87dcde4f6e88d89d1844a7
root <gnnrbachelor@gmail.com>
2022-02-19 05:48:22 +0000

    Fix for out of range

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,7 @@
 
 def validUTF8(data):
     """Checks for UTF-8 compliance"""
+    data = [n & 0xFF if n >= 0 else n for n in data]
     try:
         # bytes.decode() default args included for clarity
         bytes(data).decode(encoding='utf-8', errors='strict')
