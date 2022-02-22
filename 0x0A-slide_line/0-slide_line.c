3ac91b23109d7eafad238d1949e654c64950905a
root <gnnrbachelor@gmail.com>
2022-02-22 07:23:16 +0000

    Add slideline

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,16 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - 2048 Game implementation
+ * @line: Line
+ * @size: Size
+ * @direction: Direction
+ * Return: 1 or 0
+ */
+
+
+int slide_line(int *line, size_t size, int direction)
+{
+}
+
+
