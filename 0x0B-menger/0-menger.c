a61480a85c1bdb08161a305cef316c0575053afd
root <gnnrbachelor@gmail.com>
2022-03-02 14:59:49 +0000

    First version

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,37 @@
+#include "menger.h"
+
+/**
+ * menger - Prints menger sponge
+ * @level: levels deep
+ * Return: Void
+ */
+
+void menger(int level)
+{
+	int row, col, empty, flag;
+	int size = pow(3, level);
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			for (empty = 1; empty < size; empty *= 3)
+			{
+				if ((row / empty) % 3 == 1 && (col / empty) % 3 == 1)
+				{
+					flag = 0;
+					break;
+				}
+				else
+					flag = 1;
+
+			}
+			if (flag == 0)
+				putchar(' ');
+			else
+				putchar('#');
+
+		}
+		putchar('\n');
+	}
+}
