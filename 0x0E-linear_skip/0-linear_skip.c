4bcd269ba61b131b6f7bfcc02a1becdb184ebab7
root <gnnrbachelor@gmail.com>
2022-03-30 20:24:46 +0000

    Setup

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,14 @@
+#include "search.h"
+
+/**
+ * linear_skip - search for value in skip list
+ * @list: pointer to head
+ * @value: value
+ *
+ * Return: pointer to found node
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+}
+
+
