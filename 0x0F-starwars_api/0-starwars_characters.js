bd4da14c05abada5acabea5b582f15067cfc27eb
gnnrbachelor <gnnrbachelor@gmail.com>
2022-04-05 16:17:23 +0000

    Add task 0

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * Prints character namese
+ * First argument is Movie ID
+ */
+
+
+const request = require('request');
+
+function getCharacterName (characters, i) {
+  if (i === characters.length) return;
+  request(characters[i], (err, resp, body) => {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.parse(body).name);
+      getCharacterName(characters, ++i);
+    }
+  });
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
+  function (err, resp, body) {
+    if (err) console.log(err);
+    else {
+      const characters = JSON.parse(body).characters;
+      getCharacterName(characters, 0);
+    }
+  });
