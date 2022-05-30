0173db4eac03e98a4cb7d93956a3d63acb01d7d4
root <gnnrbachelor@gmail.com>
2022-05-30 15:53:15 +0000

    Add task 0

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_list - Finds loop in linked list
+ * @head: Head
+ * Return: Address of loop beginning or NULL
+ *
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *hare, *tortoise = head;
+
+	if (!head || !head->next)
+		return (NULL);
+
+	while (hare && hare->next)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
+}
+
